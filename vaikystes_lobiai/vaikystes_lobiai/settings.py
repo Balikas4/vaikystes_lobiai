f60cc46ea9bb4d4c19926d0a0d6c1b21a43634ae
Balikas <balikas4@gmail.com>
2024-08-31 14:25:57 +0300

    edit: settings.py

--- vaikystes_lobiai/vaikystes_lobiai/settings.py
+++ vaikystes_lobiai/vaikystes_lobiai/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'vaikystes_lobiai.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'vaikystes_lobiai/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
