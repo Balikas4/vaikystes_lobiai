0a94794a8b7d996c7d7f75c109039a97e211501a
Balikas <balikas4@gmail.com>
2024-09-10 14:39:04 +0300

    edit: settigs.py

--- vaikystes_lobiai/vaikystes_lobiai/settings.py
+++ vaikystes_lobiai/vaikystes_lobiai/settings.py
@@ -44,6 +44,7 @@ CSRF_TRUSTED_ORIGINS = ['https://vaikysteslobiai.lt', 'https://www.vaikysteslobi
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
