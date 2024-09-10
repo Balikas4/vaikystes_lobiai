ae67b33753900fb14135df9facd147869202d88f
Balikas <balikas4@gmail.com>
2024-09-10 15:54:24 +0300

    edit: settings

--- vaikystes_lobiai/vaikystes_lobiai/settings.py
+++ vaikystes_lobiai/vaikystes_lobiai/settings.py
@@ -148,10 +148,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # SMTP Server Settings
-EMAIL_HOST = '176.118.198.148'  # Replace with BaCloud SMTP server address
-EMAIL_PORT = 465  # Use 465 for SSL, 25 for non-encrypted, 587 for TLS
-EMAIL_USE_SSL = True  # Set to True if using port 587
-EMAIL_USE_TLS = False
+EMAIL_HOST = 'mail.vaikysteslobiai.lt'  # Replace with BaCloud SMTP server address
+EMAIL_PORT = 587  # Use 465 for SSL, 25 for non-encrypted, 587 for TLS
+EMAIL_USE_SSL = False  # Set to True if using port 587
+EMAIL_USE_TLS = True
 
 
 # Authentication Settings
